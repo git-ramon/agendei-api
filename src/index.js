eff4787b0a5b6d96ff4d4758dbf15e2027dbe0f6
Ramon Rodrigues <ramonrodriguesnw@gmail.com>
2024-11-09 13:06:19 -0300

    permição de cabeçalhos adicionais

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const app = express();
 const corsOptions = {
     origin: 'https://agendei-web-mauve.vercel.app',  // Permitir apenas essa origem
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir cabeçalhos
+    allowedHeaders: ['Content-Type', 'Authorization', '*'],  // Permitir cabeçalhos
 };
 
 // Aplicar CORS antes de qualquer outra configuração de rota
 app.use(cors(corsOptions));
 
 app.use(express.json());
-//app.use(cors());
 app.use(router);
+//app.use(cors());
 
 // Use process.env.PORT para Vercel
 const PORT = process.env.PORT || 3001; // Usar 3001 localmente
