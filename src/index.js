bf78c04c52fe5090c76235239b47e6496ce2a116
Ramon Rodrigues <ramonrodriguesnw@gmail.com>
2024-11-09 01:46:36 -0300

    remoção de require

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import express from "express";
 import cors from "cors";
 import router from "./routes.js";
 
-require('dotenv').config();
-
 const app = express();
 
 // Configuração do CORS com a origem específica para a sua aplicação web
