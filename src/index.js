8f7d16e46c607450428a2c622513fd19fa0970a0
Ramon Rodrigues <ramonrodriguesnw@gmail.com>
2024-11-09 00:55:43 -0300

    config. Cors

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ require('dotenv').config();
 
 const app = express();
 
+// Configuração do CORS com a origem específica para a sua aplicação web
+const corsOptions = {
+    origin: 'https://agendei-web-mauve.vercel.app',  // Permitir apenas essa origem
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],  // Permitir cabeçalhos
+};
+
+// Aplicar CORS antes de qualquer outra configuração de rota
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(cors());
 app.use(router);
